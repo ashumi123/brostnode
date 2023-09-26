01780249d1feff02105c8cb3a791799c15711e40
ashumi123 <ashwanimittal@Ashwanis-MacBook-Air.local>
2023-09-26 20:24:07 +0530

    cors

--- bin/www.js
+++ bin/www.js
@@ -42,13 +42,13 @@ const onListening = () => {
 const port = normalizePort(process.env.PORT || '4000');
 // app.set('port', port);
 const corsOptions = {
-  origin: 'https://clubboxx.in',
+  origin: 'https://clubboxx.in/',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 console.log('server listening on port :::: ', port)
 
